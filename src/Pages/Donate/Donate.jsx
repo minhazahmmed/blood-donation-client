9cae8b63b4a6818bab16184ca4659bd7e16b40b4
minhazahmmed <minhazahmmed231@gmail.com>
2025-12-21 16:17:26 +0600

    Payment success added

--- src/Pages/Donate/Donate.jsx
+++ src/Pages/Donate/Donate.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import useAxios from '../../hooks/useAxios';
+import { useContext } from 'react';
+import { AuthContext } from '../../Provider/AuthProvider';
+import { useNavigate } from 'react-router';
 
 const Donate = () => {
+    const axiosInstance = useAxios();
+    const {user} = useContext(AuthContext);
+    const navigate = useNavigate();
+
+
+    const handleCheckout = (e)=> {
+        e.preventDefault();
+
+        const donateAmount = e.target.donateAmount.value;
+        const donorEmail = user?.email;
+        const donorName = user?.displayName;
+        
+        const formData = {
+            donateAmount,
+            donorEmail,
+            donorName,
+
+            
+        }
+
+        axiosInstance.post('/create-payment-checkout', formData)
+        .then(res => {
+            console.log(res.data)
+        window.location.href = res.data.url
+        })
+    }
     return (
         <div>
-            <form action="" className='flex justify-center items-center min-h-screen gap-5'>
-               <input type="text" placeholder="Type here" className="input" />
+            <form onSubmit={handleCheckout} className='flex justify-center items-center min-h-screen gap-5'>
+               <input name='donateAmount' type="text" placeholder="Type here" className="input" />
                 <button className='btn btn-primary' type='submit'>Donate</button>
             </form>
         </div>
